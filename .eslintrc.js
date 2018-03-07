944289bae05ce677bb84c87b440fa8dc850a26ac
pengchaoyang <pengchaoyang@sogou-inc.com>
2018-03-07 20:32:12 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        }
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+// const str = 'some value'
+
+function fn(){
+    // console.log('some log')
+}
+fn()
+// console.log(str)
